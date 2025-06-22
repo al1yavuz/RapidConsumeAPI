33709944c6ca93a11b3c6b3b0dc89dbc1228114b
Ali Yavuz <ali.yavuz7734@gmail.com>
2025-06-22 14:30:57 +0300

    test: Swagger UI login and API testing completed

---
+++ RapidConsumeAPI/ApiConsume/HotelProject.WebApi/Controllers/RoomController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult RoomList()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult AddRoom()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteRoom()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateRoom()
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")] //dışarıdan bir id parametresi alıcak.
+        public IActionResult GetRoom()
+        {
+            return Ok();
+        }
+    }
+}
