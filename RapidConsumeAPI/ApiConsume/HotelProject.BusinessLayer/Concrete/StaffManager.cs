78046c99acdf27d8698f1d650382a8613aaacd4f
Ali Yavuz <ali.yavuz7734@gmail.com>
2025-06-22 12:36:40 +0300

    Completed implementation of manager classes

---
+++ RapidConsumeAPI/ApiConsume/HotelProject.BusinessLayer/Concrete/StaffManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class StaffManager : IStaffService
+    {
+        private readonly IStaffDal _IStaffDal;
+
+        public StaffManager(IStaffDal ıstaffDal)
+        {
+            _IStaffDal = ıstaffDal;
+        }
+
+        public void TDelete(Staff t)
+        {
+            _IStaffDal.Delete(t);
+        }
+
+        public Staff TGetByID(int id)
+        {
+            return _IStaffDal.GetByID(id);
+        }
+
+        public List<Staff> TGetList()
+        {
+            return _IStaffDal.GetList();
+        }
+
+        public void TInsert(Staff t)
+        {
+            _IStaffDal.Insert(t);
+        }
+
+        public void TUpdate(Staff t)
+        {
+            _IStaffDal.Update(t);
+        }
+    }
+}
