cbd818c7bb7896974d09327c5162cd8a04fc271d
Ali Yavuz <ali.yavuz7734@gmail.com>
2025-06-27 13:48:05 +0300

    feat: Enable CORS and implement external API consume functionality

--- RapidConsumeAPI/Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ RapidConsumeAPI/Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class StaffController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StaffController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient(); //consume edilmesi için istemci oluşturdum.
+            var responseMessage = await client.GetAsync("http://localhost:5093/api/Staff");//daha sonra bu adrese istekte bulundum.
+            if (responseMessage.IsSuccessStatusCode) //eğer adresten başarılı bir durum kodu dönerse gelen veriyi jsonData değişkenine atadım
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
