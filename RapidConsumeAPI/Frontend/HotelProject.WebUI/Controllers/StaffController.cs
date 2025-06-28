ce6b2ed974167a0c9804598ebc19bfaa7dadd457
Ali Yavuz <ali.yavuz7734@gmail.com>
2025-06-28 14:37:54 +0300

    feat: Implement staff delete and update functionality

--- RapidConsumeAPI/Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ RapidConsumeAPI/Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -44,6 +44,43 @@ namespace HotelProject.WebUI.Controllers
             }
             return View();
         }
+        public async Task <IActionResult> DeleteStaff(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:5093/api/Staff/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateStaff(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5093/api/Staff/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateStaffViewModel>(jsonData);
+                return View(values);
+            }
+            return View();
 
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateStaff(UpdateStaffViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8,"application/json");
+            var responseMessage = await client.PutAsync("http://localhost:5093/api/Staff/" , stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
     }
 }
