c1cf60c05e78d91bfd6fb0c0c610b3740ee49ae1
Ali Yavuz <ali.yavuz7734@gmail.com>
2025-06-29 13:50:57 +0300

    feat: Add Room and UpdateRoom DTOs and configure AutoMapper

---
+++ RapidConsumeAPI/ApiConsume/HotelProject.WebApi/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.WebApi.Mapping
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<RoomAddDto,Room>();   //mapleme işlemi sayesinde dto sınıfına geçmiş olduğumuz propertyler entity katmanında yer alan propertyler eşleşmiş olucak.
+            CreateMap<Room, RoomAddDto>();
+
+            CreateMap<UpdateRoomDto,Room>().ReverseMap(); //reversemap dediğimde kendisi tersine mapleme işini yapıyor.
+        }
+    }
+}
