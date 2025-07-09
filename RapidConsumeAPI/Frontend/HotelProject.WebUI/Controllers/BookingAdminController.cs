02451e473895ae2ffd28324f51a13233569ba307
Ali Yavuz <ali.yavuz7734@gmail.com>
2025-07-09 18:35:40 +0300

    feat: Add admin booking list page with ResultBookingDto

---
+++ RapidConsumeAPI/Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -0,0 +1,29 @@
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class BookingAdminController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingAdminController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient(); //consume edilmesi için istemci oluşturdum.
+            var responseMessage = await client.GetAsync("http://localhost:5093/api/Booking");//daha sonra bu adrese istekte bulundum.
+            if (responseMessage.IsSuccessStatusCode) //eğer adresten başarılı bir durum kodu dönerse gelen veriyi jsonData değişkenine atadım
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
