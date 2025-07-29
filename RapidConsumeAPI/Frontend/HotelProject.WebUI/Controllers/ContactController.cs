af813029b5aaf8d021cdd27f98ab7384ff788fe0
Ali Yavuz <ali.yavuz7734@gmail.com>
2025-07-29 18:16:52 +0300

    feat: Complete contact page UI and test contact endpoint on Swagger

--- RapidConsumeAPI/Frontend/HotelProject.WebUI/Controllers/ContactController.cs
+++ RapidConsumeAPI/Frontend/HotelProject.WebUI/Controllers/ContactController.cs
@@ -1,11 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using HotelProject.WebUI.Dtos.ContactDto;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult SendMessage()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(CreateContactDto createContactDto)
+        {
+            createContactDto.Date = DateTime.Parse(DateTime.Now.ToShortDateString());           
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5093/api/Contact", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
